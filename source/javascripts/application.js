3484868aec5a47cd175b2011bca0c835cb9889b0
Mateo Ceron <mateo-ceron@hotmail.fr>
2019-02-04 12:22:48 +0100

    test

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,24 +1,4 @@
 //= require jquery
 //= require bootstrap
 //= require_tree .
-document.addEventListener('DOMContentLoaded', function(){
-  let counter = 0;
-  const carousselItems = document.querySelectorAll('.js-caroussel');
 
-  setInterval(function(){
-
-    if (counter >= carousselItems.length - 1) {
-      counter = 0;
-    } else {
-      counter++;
-    }
-
-    for (let i = 0; i < carousselItems.length; i++) {
-      if (carousselItems[i] != carousselItems[counter]) {
-        carousselItems[i].classList.remove('is-visible');
-      }
-    }
-    carousselItems[counter].classList.add('is-visible');
-  }, 5000);
-
-}) // Fin du DOM
