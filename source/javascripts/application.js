f384570e038e5585e4ed2e596257ccc8a2f50d71
Mateo Ceron <mateo-ceron@hotmail.fr>
2019-02-04 12:07:24 +0100

    responsive 2.0

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,24 +1,9 @@
 //= require jquery
 //= require bootstrap
 //= require_tree .
-document.addEventListener('DOMContentLoaded', function(){
-  let counter = 0;
-  const carousselItems = document.querySelectorAll('.js-caroussel');
+$(document).ready(function(){
+  // $('.container').carousel();
+  $('.cookieinfo-close').css('')
 
-  setInterval(function(){
-
-    if (counter >= carousselItems.length - 1) {
-      counter = 0;
-    } else {
-      counter++;
-    }
-
-    for (let i = 0; i < carousselItems.length; i++) {
-      if (carousselItems[i] != carousselItems[counter]) {
-        carousselItems[i].classList.remove('is-visible');
-      }
-    }
-    carousselItems[counter].classList.add('is-visible');
-  }, 5000);
 
 }) // Fin du DOM
